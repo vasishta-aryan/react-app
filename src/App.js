63cb95b77fba5ab1bda9766a74b700a9f8ef04c6
Aryan Vasishta <89566020+vasishta-aryan@users.noreply.github.com>
2021-12-02 10:53:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sidharath Bansal
+          Aryan Vasishta
         </p>
         <a
           className="App-link"
